193eebd9c1be0b77370ae28a747a0956a93caafd
Samuel Gagnon <Samuel@Samuels-MacBook-Pro.local>
2015-11-08 22:13:01 -0500

    missing information added, video not wroking

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,6 @@
 angular.module('cornpub', ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'angularUtils.directives.dirPagination'])
-  .constant('baseURL', "http://localhost:3000")
-  //.constant('baseURL', "https://umovie.herokuapp.com")
+  //.constant('baseURL', "http://localhost:3000")
+  .constant('baseURL', "https://umovie.herokuapp.com")
 
   .config(function($logProvider) {
     'use strict';
